c0fe26da5f8afe4f3b0fceebb3975b8f8779a91b
AdeelAmad <57554191+AdeelAmad@users.noreply.github.com>
2023-09-17 21:11:17 -0700

    Update signals.py

--- track/signals.py
+++ track/signals.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.dispatch import receiver
 from .models import tracker
 import easypost
-easypost.api_key = "EZTK38cba7fc98ee4b78904dd204527d47edn0nf2wQbyDW3RxBuhCyXzw"
+easypost.api_key = ""
 
 @receiver(post_save, sender=tracker)
 def create_tracker(sender, instance, created, *args ,**kwargs):
